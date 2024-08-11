b44f31069952660ff7cee695504a6f49e2be01d6
Cleidson <cleidson.mac@outlook.com>
2024-08-11 15:00:25 +0100

    Basic Start Project With a Basic Product Class Test

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product-Page</title>
+</head>
+<body>
+    <h1>Product</h1>
+    <div>Index</div>
+    <h1>YOU ARE THE BEST MDF EVER!! BRO!!!</h1>
+</body>
+</html>
