89cc0563f5775279846f369ce20fdf74527b99bf
Cleidson <cleidson.mac@outlook.com>
2024-08-12 11:14:54 +0100

    Add Blade Package for Simple Login Feature

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/create', [ProductController::class, 'createPage'])->name('product.create');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-/* CRUD METHODS */
-Route::post('/product', [ProductController::class, 'saveOne'])->name('product.saveOne');
-Route::get('/product/{product}/edit', [ProductController::class, 'editOne'])->name('product.editOne');
-Route::put('/product/{product}/update', [ProductController::class, 'updateOne'])->name('product.updateOne');
-Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
